045f76abc307ffa329443cfdc9f38be0d2291379
Teodora Georgieva <teodora.georgieva@taryafintech.com>
2021-05-17 10:55:58 +0300

    test 1

--- src/test/Student.java
+++ src/test/Student.java
@@ -1,10 +1,29 @@
 package test;
 
+import java.util.List;
+
 public class Student extends Person{
     private String schoolName;
+    private List<Integer> grades;
 
     public Student(String name, int age, double height, String schoolName) {
         super(name, age, height);
         this.schoolName = schoolName;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    public List<Integer> getGrades() {
+        return grades;
+    }
+
+    public void setGrades(List<Integer> grades) {
+        this.grades = grades;
     }
 }
